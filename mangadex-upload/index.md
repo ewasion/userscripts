e6b7044adb930e59294199668afb142cb3c19f1e
‮noisawe <3evasion@gmail.com>
2018-02-27 22:46:03 +0100

    Added Mangadex uploader

---
+++ mangadex-upload/index.md
@@ -0,0 +1,5 @@
+## MangaDex uploader
+
+> Highly customizable upload script for MangaDex
+
+[Install](https://raw.githubusercontent.com/ewasion/userscripts/master/mangadex-upload/mangadex-upload.user.js) (Greasemonkey/Tampermonkey/Violentmonkey required)
---
+++ mangadex-upload/mangadex-upload.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         MangaDex upload
+// @namespace    https://github.com/ewasion
+// @version      0.0.0
+// @license      CC-BY-NC-SA-4.0
+// @description  Highly customizable upload script for MangaDex
+// @author       Eva
+// @match        https://mangadex.com/upload/*
+// @icon         https://mangadex.com/favicon.ico
+// @homepage     https://ewasion.github.io/userscripts/mangadex-upload/
+// @updateURL    https://raw.githubusercontent.com/ewasion/userscripts/master/mangadex-upload/mangadex-upload.meta.js
+// @downloadURL  https://raw.githubusercontent.com/ewasion/userscripts/master/mangadex-upload/mangadex-upload.user.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+  'use strict';
+})();
